861c263b69535350c34aa894b38722a79cb497af
KishoreJohnsan <kishorejohnsan.s@gmail.com>
2020-05-31 23:50:56 +0530

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    purge: [
+      '*.html',     
+    ],
+    theme: {
+        fontFamily: {
+            display: ['Gilroy', 'sans-serif'],
+            body: ['Manrope', 'sans-serif'],
+          },
+    },
+    variants: {},
+    plugins: [],
+  }
