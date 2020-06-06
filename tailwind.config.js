b81f313e012f8af4ba7210085b311380a64197a0
KishoreJohnsan <kishorejohnsan.s@gmail.com>
2020-06-07 00:40:54 +0530

    added dark mode

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,11 @@ module.exports = {
       '*.html',     
     ],
     theme: {
+      extend: {
+        screens: {
+          'dark-mode': { raw: '(prefers-color-scheme: dark)' }
+        }
+      },
         fontFamily: {
             display: ['Gilroy', 'sans-serif'],
             body: ['Manrope', 'sans-serif'],
